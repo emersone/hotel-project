7da36ecb86e2431639138f23374348316c4d7a6e
emersone <you@example.com>
2019-02-12 15:25:31 -0800

    Added classes to main

--- main.py
+++ main.py
@@ -1,15 +1,52 @@
+#Source: Youtuber Corey Schafer's Python Flask Tutorial series
+
 from flask import Flask, render_template, current_app, request, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from forms import RegisterForm, LoginForm
+from datetime import datetime
 import logging
-app = Flask(__name__)
 
+app = Flask(__name__)
 app.config['SECRET_KEY'] = '89rAZOpeZSqGTsYal0z9'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
 
-#Base code from: Youtuber Corey Schafer's Python Flask Tutorial series
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(20), unique = True, nullable = False)
+    email = db.Column(db.String(120), unique = True, nullable = False)
+    avatar = db.Column(db.String(20), nullable = False, default = 'default.jpg')
+    password = db.Column(db.String(60), nullable = False)
+    reservations = db.relationship('Reservation', backref = 'owner', lazy = True)
+
+    #how object is printed
+    def __repr__(self):
+        return f"User('{self.username}, {self.email}, {self.avatar}')"
+
+class Hotel(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(120), nullable = False)
+    city = db.Column(db.String(120), nullable = False)
+    state = db.Column(db.String(120), nullable = False)
+    country = db.Column(db.String(120), nullable = False)
+    rating = db.Column(db.Integer, nullable = False)
+    price_cat = db.Column(db.String(5), nullable = False)
+    #content = db.Column(db.Text, nullable = False)
+
+    #how object is printed
+    def __repr__(self):
+        return f"Hotel('{self.name}, {self.city}, {self.state}, {self.country}, {self.rating}, {self.price_cat}, {self.content}')"
+
+class Reservation(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    date_made = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
+
+    #how object is printed
+    def __repr__(self):
+        return f"Reservation('{self.date_made}', {self.user_id})"
+
 
 
 
